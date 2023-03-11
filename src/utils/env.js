f64db29051760781aafbcc8240223531b9e3175c
fan9704 <cxz123499@gmail.com>
2023-03-11 22:53:00 +0800

    Env Complete

---
+++ src/utils/env.js
@@ -0,0 +1,10 @@
+const API_KEY = process.env["API_KEY"]
+const AUTH_DOMAIN = process.env["AUTH_DOMAIN"]
+const PROJECT_ID = process.env["PROJECT_ID"]
+const STORAGE_BUCKET = process.env["STORAGE_BUCKET"]
+const MESSAGING_SENDER_ID = process.env["MESSAGING_SENDER_ID"]
+const APP_ID = process.env["APP_ID"]
+const MEASUREMENT_ID = process.env["MEASUREMENT_ID"]
+
+
+export { API_KEY, AUTH_DOMAIN, PROJECT_ID, STORAGE_BUCKET, MESSAGING_SENDER_ID, APP_ID, MEASUREMENT_ID }
---
+++ src/utils/firebaseCore.js
@@ -0,0 +1,23 @@
+import { getAuth, GithubAuthProvider } from "firebase/auth";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { API_KEY } from "./env";
+
+const firebaseConfig = {
+    apiKey: "A=IzaSyApi1UE5zrhK2FVfGyKE70nAsxTLmw9Xj8",
+    authDomain: "vuefireauth-94879.firebaseapp.com",
+    projectId: "vuefireauth-94879",
+    storageBucket: "vuefireauth-94879.appspot.com",
+    messagingSenderId: "941502028048",
+    appId: "1:941502028048:web:3275c25e3331b85c9fad95",
+    measurementId: "G-7CM1RSK215"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth(app);
+
+const githubProvider = new GithubAuthProvider();
+
+export { auth, githubProvider }
